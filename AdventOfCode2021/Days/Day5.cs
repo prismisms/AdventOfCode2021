f2582958ad9b084edef682a7d0215e494c2255f0
Natalie <radioprotector@gmail.com>
2021-12-05 10:27:58 +0000

    oop

--- AdventOfCode2021/Days/Day5.cs
+++ AdventOfCode2021/Days/Day5.cs
@@ -55,13 +55,14 @@ namespace AdventOfCode2021
             var dx = Math.Sign(coord.X2 - coord.X1);
             var dy = Math.Sign(coord.Y2 - coord.Y1);
             var steps = Math.Max(Math.Abs(coord.X2 - coord.X1), Math.Abs(coord.Y2 - coord.Y1)) + 1;
+            var x = coord.X1;
+            var y = coord.Y1;
 
             for (var i = 1; i <= steps; ++i)
             {
-                yield return (coord.X1, coord.Y1);
+                yield return (x, y);
 
-                coord.X1 = coord.X1 == coord.X2 ? coord.X2 : coord.X1 + dx;
-                coord.Y1 = coord.Y1 == coord.Y2 ? coord.Y2 : coord.Y1 + dy;
+                y = y == coord.Y2 ? coord.Y2 : y + dy;
             }
         }
 
