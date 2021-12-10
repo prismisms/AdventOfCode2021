5e55d3d4e5f46afd8714076533c2f1908894f024
Natalie <radioprotector@gmail.com>
2021-12-10 10:36:29 +0000

    day 10 part 2

--- AdventOfCode2021/Days/Day10.cs
+++ AdventOfCode2021/Days/Day10.cs
@@ -1,13 +1,13 @@
 ﻿using System;
 namespace AdventOfCode2021
 {
-    public class Day10 : DayBase<string, int>
+    public class Day10 : DayBase<string, long>
     {
-        private readonly Dictionary<char, int> _pointsRef = SetPoints();
+        private readonly Dictionary<char, int> _illegalScores = SetIllegalScores();
+        private readonly Dictionary<char, int> _autocompleteScores = SetAutocompleteScores();
 
         private readonly string _leftBrackets = "{[(<";
         private readonly string _rightBrackets = "}])>";
-        private readonly string _allBrackets = "{[(<}])>";
 
         private readonly List<string> bracketPairs = new List<string>
         {
@@ -21,7 +21,7 @@ namespace AdventOfCode2021
         {
         }
 
-        public override int Part1()
+        public override long Part1()
         {
             var illegalBracketValue = 0;
             FileContents.ForEach(line =>
@@ -35,12 +35,52 @@ namespace AdventOfCode2021
                 else
                 {
                     var firstIllegalCharacter = remainingLine.First(x => _rightBrackets.Contains(x));
-                    illegalBracketValue += _pointsRef[firstIllegalCharacter];
+                    illegalBracketValue += _illegalScores[firstIllegalCharacter];
                 }
       
             });
 
-            return illegalBracketValue;
+            return (long)illegalBracketValue;
+        }
+
+        public override long Part2()
+        {
+
+            var incompleteLines = GetIncompleteLines();
+
+            var incompleteLineScores = incompleteLines.Select(line =>
+            {
+                var value = (long)0;
+
+                // Holy shit finally a situation where I need to reverse a string
+                var reversedLine = Reverse(line);
+
+                reversedLine.ToList().ForEach(character =>
+                { 
+                    value = (value * 5) + _autocompleteScores[character];
+                });
+
+                return value;
+
+            }).ToList().OrderByDescending(x => x).ToList();
+
+            return incompleteLineScores[(incompleteLineScores.Count - 1) / 2]);
+        }
+
+        private List<string> GetIncompleteLines()
+        {
+            var incompleteLines = new List<string>();
+            FileContents.ForEach(line =>
+            {
+                var remainingLine = RemoveChunks(line);
+                if (remainingLine.All(x => _leftBrackets.Contains(x)))
+                {
+                    // This is just an incomplete line
+                    incompleteLines.Add(remainingLine);
+                }
+            });
+
+            return incompleteLines;
         }
 
         private string RemoveChunks(string line)
@@ -58,12 +98,14 @@ namespace AdventOfCode2021
             return line;
         }
 
-        public override int Part2()
+        private static string Reverse(string s)
         {
-            return 0;
+            char[] charArray = s.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
         }
 
-        private static Dictionary<char, int> SetPoints()
+        private static Dictionary<char, int> SetIllegalScores()
         {
             return new Dictionary<char, int>
             {
@@ -75,6 +117,17 @@ namespace AdventOfCode2021
                 { '[', 57 },
                 { '{', 1197 },
                 { '<', 25137 }
+            };
+        }
+
+        private static Dictionary<char, int> SetAutocompleteScores()
+        {
+            return new Dictionary<char, int>()
+            {
+                { '(', 1 },
+                { '[', 2 },
+                { '{', 3 },
+                { '<', 4 }
             };
         }
     }
